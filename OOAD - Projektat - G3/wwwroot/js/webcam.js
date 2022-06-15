01e483f911010c19823792dddd28b84d4b522531
akulaglic3 <akulaglic3@etf.unsa.ba>
2022-06-15 21:01:32 +0200

    Finalni push

--- OOAD - Projektat - G3/wwwroot/js/webcam.js
+++ OOAD - Projektat - G3/wwwroot/js/webcam.js
@@ -1,4 +1,4 @@
-﻿// WebcamJS v1.0.26
+// WebcamJS v1.0.26
 // Webcam library for capturing JPEG/PNG images in JavaScript
 // Attempts getUserMedia, falls back to Flash
 // Author: Joseph Huckaby: http://github.com/jhuckaby
